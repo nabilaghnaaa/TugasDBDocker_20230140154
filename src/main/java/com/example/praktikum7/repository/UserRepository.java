06ad73b57a4691d259b4a98cd990b490ca39dc0e
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2026-04-21 10:02:12 +0700

    membuat repository JPA untuk mengelola data User di database

--- src/main/java/com/example/praktikum7/repository/UserRepository.java
+++ src/main/java/com/example/praktikum7/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.praktikum7.repository;
 
-public interface UserRepository {
+import com.example.praktikum7.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, String> {
 }
