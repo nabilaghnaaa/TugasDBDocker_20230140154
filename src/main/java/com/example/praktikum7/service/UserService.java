09aecba56c2db1b618d217f4b3bc1d56449cb6dd
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2026-04-21 10:10:14 +0700

    membuat lapisan service di Spring Boot yang bertugas menangani logika bisnis saat menambahkan user baru ke database.

--- src/main/java/com/example/praktikum7/service/UserService.java
+++ src/main/java/com/example/praktikum7/service/UserService.java
@@ -1,4 +1,22 @@
 package com.example.praktikum7.service;
 
+import com.example.praktikum7.model.User;
+import com.example.praktikum7.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request){
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
 }
