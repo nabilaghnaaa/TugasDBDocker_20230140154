c578ba95f5e1bcba8eaaba1fceb1a2e057bc589d
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2026-04-21 10:14:40 +0700

    menambahkan fungsi service untuk mendapatkan user berdasarkan id nya

--- src/main/java/com/example/praktikum7/service/UserService.java
+++ src/main/java/com/example/praktikum7/service/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User getUserById(String id){
+        return userRepository.findById(id).orElse(null);
+    }
+    
 }
