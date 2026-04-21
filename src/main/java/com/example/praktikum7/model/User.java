12a569c433b6d3b70c2ef7708574ac5a17d360b1
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2026-04-21 09:38:40 +0700

    Initial commit

---
+++ src/main/java/com/example/praktikum7/model/User.java
@@ -0,0 +1,29 @@
+package com.example.praktikum7.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "20230140154_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+}
